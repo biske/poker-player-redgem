c7cdeddd240ba9a49598549cece4872453ee1d6d
Иван Бишевац <ivan.bisevac@gmail.com>
2015-09-05 15:17:42 +0200

    Multiply index with 1000

--- player.rb
+++ player.rb
@@ -9,19 +9,15 @@ class Player
   MINIMUM_RAISE = 20
 
   def bet_request(game_state)
-    puts game_state.class.inspect
-    puts game_state.inspect
-
     me = game_state["players"][game_state["in_action"]]
     card1 = me["hole_cards"][0]
     card2 = me["hole_cards"][1]
 
     index = CardCombinations.new(card1, card2).calculate_index
     
-    puts index.inspect
-    puts me["stack"]
+    puts "index: #{index.inspect}"
 
-    index * me["stack"]  
+    index * 1000
   end
 
   def showdown(game_state)
