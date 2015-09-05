737f23809901d28d1d3a7aa18f2d43649e0e3a31
Иван Бишевац <ivan.bisevac@gmail.com>
2015-09-05 12:30:41 +0200

    Adds game_state to log

--- player.rb
+++ player.rb
@@ -8,7 +8,8 @@ class Player
   def bet_request(game_state)
     game_state = JSON.parse(game_state)
 
-    
+    puts game_state.inspect
+
     my_hole_cards = game_state["players"][game_state["in_action"]]["hole_cards"]
 
     if (my_hole_cards[0]["rank"] == my_hole_cards[1]["rank"]) or (my_hole_cards[0]["suit"] == my_hole_cards[1]["suit"])
