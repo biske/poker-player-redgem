be2babee77ad243436751a756272e2b8ca7b21ca
Иван Бишевац <ivan.bisevac@gmail.com>
2015-09-05 12:20:30 +0200

    Adds same cards check

--- player.rb
+++ player.rb
@@ -1,10 +1,21 @@
+require 'json'
+require 'pry'
+
 
 class Player
-
   VERSION = "Default Ruby folding player"
 
   def bet_request(game_state)
-    200
+    game_state = JSON.parse(game_state)
+
+    
+    my_hole_cards = game_state["players"][1]["hole_cards"]
+
+    if my_hole_cards[0]["rank"] == my_hole_cards[1]["rank"]
+      500
+    else
+      200
+    end
   end
 
   def showdown(game_state)
